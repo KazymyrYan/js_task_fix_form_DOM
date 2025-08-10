a59f44ce62f403bdde59c339883b5edc37c0ffe9
Yan Kazymyr <kazymyr.yan@gmail.com>
2025-08-10 16:22:00 +0300

    Improved

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,14 +4,14 @@ const inputs = document.querySelectorAll('form input');
 
 inputs.forEach((input) => {
   const label = document.createElement('label');
+  const formattedText = formatText(input.name);
 
   label.className = 'field-label';
   label.htmlFor = input.id;
-  label.textContent = formatText(input.name);
+  label.textContent = formattedText;
 
   input.parentElement.insertBefore(label, input);
-
-  input.placeholder = formatText(input.name);
+  input.placeholder = formattedText;
 });
 
 function formatText(str) {
