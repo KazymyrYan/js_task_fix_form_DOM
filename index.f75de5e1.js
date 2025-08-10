f3150f0ee659a84def211dae6ca68352b293e158
Yan Kazymyr <kazymyr.yan@gmail.com>
2025-08-10 16:22:21 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,18 @@
+"use strict";
+const inputs = document.querySelectorAll("form input");
+inputs.forEach((input)=>{
+    const label = document.createElement("label");
+    const formattedText = formatText(input.name);
+    label.className = "field-label";
+    label.htmlFor = input.id;
+    label.textContent = formattedText;
+    input.parentElement.insertBefore(label, input);
+    input.placeholder = formattedText;
+});
+function formatText(str) {
+    const replaced = str.replace(/-/g, " ");
+    const withSpaces = replaced.replace(/([A-Z])/g, " $1").trim();
+    return withSpaces[0].toUpperCase() + withSpaces.slice(1).toLowerCase();
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
